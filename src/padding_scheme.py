7870ddafba5626fb48c3181811a23dab55396e1f
Thomas Taylor <thomas.john.taylor@outlook.com>
2024-02-14 15:07:11 +1100

    wrote function to remove padding

--- src/padding_scheme.py
+++ src/padding_scheme.py
@@ -9,13 +9,15 @@ def add_padding(data: bytes, start_after: bytes = b'\x00', chunk_size = 24, payl
                 data[:payload_max],\
                 start_after=start_after,\
                 chunk_size=chunk_size,\
-                payload_max=payload_max)
+                payload_max=payload_max\
+                )
 
         tail = add_padding(\
                 data[payload_max:],\
                 start_after=start_after,\
                 chunk_size=chunk_size,\
-                payload_max=payload_max)
+                payload_max=payload_max\
+                )
 
         return head + tail
 
@@ -24,11 +26,29 @@ def add_padding(data: bytes, start_after: bytes = b'\x00', chunk_size = 24, payl
 
     # make sure start_after is not in the padding
     while padding_bytes.count(start_after):        
-        padding_bytes = padding_bytes.replace(start_after, randbytes(len(start_after)))
+        padding_bytes = padding_bytes.replace(start_after, randbytes(len(start_after)), 1)
+    
+    return padding_bytes + start_after + data
 
 
     
-    pass
 
-def remove_padding():
-    pass
+
+def remove_padding(data: bytes, start_after: bytes = b'\x00', chunk_size = 24):
+
+    if len(data) > chunk_size:
+        
+        head = remove_padding(\
+                data[:chunk_size],\
+                start_after=start_after,\
+                chunk_size=chunk_size\
+                )
+
+        tail = remove_padding(\
+                data[chunk_size:],\
+                start_after=start_after,\
+                chunk_size=chunk_size\
+                )
+        return head + tail
+    return data.split(start_after,1)[-1]
+
