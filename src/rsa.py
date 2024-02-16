18e44831eaed624944984206a55977a97ae9f943
Thomas Taylor <thomas.john.taylor@outlook.com>
2024-02-16 12:20:20 +1100

    part of encrypt function added

--- src/rsa.py
+++ src/rsa.py
@@ -1,8 +1,13 @@
 from .padding_scheme import add_padding, remove_padding
 
+
 def encrypt(data: bytes, key, chunk_size = 24):
 
     padded_data = add_padding(data, chunk_size = chunk_size)
+    number_of_chunks = len(padded_data)//chunk_size
+    chunks = map(lambda k: padded_data[k*chunk_size:(k+1)*chunk_size],range(number_of_chunks))
+    as_integers = map(lambda c: int.from_bytes(c, 'big'), chunks)
+
     
 def decrypt(data: bytes, key, chunk_size = 24):
     pass
